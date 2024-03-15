2a87322a060d4b2eab6ed976d4563259da1f5b24
KernelInterrupt <254471942@qq.com>
2024-03-15 11:18:04 +0800

    update

--- bin/setup.dart
+++ bin/setup.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 Future<void> runScript() async {
   var result = await Process.run(
-    'dart',
-    ['./scripts/init_linux.sh'], // 这里的路径是相对于当前工作目录的
+    'bash',
+    ['../scripts/init_linux.sh'], // 这里的路径是相对于当前工作目录的
     // 你可以设置脚本的工作目录
   );
   print(result.stdout);
   print(result.stderr);
+  if (result.exitCode != 0) {
+  print('Script failed: exit code ${result.exitCode}');
+}
 }
 
 void main(List<String> arguments) {
--- scripts/init_linux.sh
+++ scripts/init_linux.sh
@@ -2,5 +2,5 @@ cd ..
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
 mv libtorch libtorch-linux
-ln ../ios/Classes/native_pytorch.cpp ../linux/native_pytorch.cpp
+ln /ios/Classes/native_pytorch.cpp /linux/native_pytorch.cpp
 echo "Done"
