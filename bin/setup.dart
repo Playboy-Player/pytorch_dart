e6bd3e3d94117419e6ae3618528664b5fa256b66
KernelInterrupt <254471942@qq.com>
2024-03-28 13:00:28 +0800

    update v0.0.6+7

--- bin/setup.dart
+++ bin/setup.dart
@@ -1,97 +1,100 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:archive/archive.dart';
 import 'package:package_config/package_config.dart';
 import 'package:path/path.dart' as path;
 import 'package:collection/collection.dart';
 
-Future<void> runScript4Linux() async {
- String? currentDirectory;
-  
- // 加载当前包的配置信息
-  var packageConfig = await findPackageConfig(Directory.current);
-  if (packageConfig != null) {
-    // 查找指定插件的Package对象
-    var packageName = 'pytorch_dart'; 
-    var package = packageConfig.packages.firstWhereOrNull(
-      (pkg) => pkg.name == packageName,
-    );
-    if (package != null) {
-      
-      var packageRootPath = path.fromUri(package.packageUriRoot);
-      print('plugin root path:$packageRootPath');
-      
-      currentDirectory = packageRootPath;
-      print('currect directory$currentDirectory');
-    } else {
-      print('cant find $packageName');
+Future<void> setup4Linux() async {
+// 文件的URL
+  var url = Uri.parse('https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip');
+  // 输出文件名
+  var filename = 'libtorch-shared-with-deps-latest.zip';
+
+  // 下载文件
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    // 保存下载的文件
+    var file = File(filename);
+    await file.writeAsBytes(response.bodyBytes);
+    print('File downloaded and saved as $filename');
+
+    // 读取ZIP文件
+    var bytes = file.readAsBytesSync();
+
+    // 解压缩文件内容
+    var archive = ZipDecoder().decodeBytes(bytes);
+
+    // 从压缩包中提取文件
+    for (var file in archive) {
+      var filename = file.name;
+      if (file.isFile) {
+        var data = file.content as List<int>;
+        File(filename)
+          ..createSync(recursive: true)
+          ..writeAsBytesSync(data);
+      } else {
+        Directory(filename).createSync(recursive: true);
+      }
     }
+    print('Files extracted');
+
+    // 删除下载的ZIP文件
+    await file.delete();
+    print('ZIP file deleted');
   } else {
-    print('cant find packageConfig');
+    print('Failed to download file: ${response.statusCode}');
   }
-  
-  
-  ProcessStartMode processStartMode = ProcessStartMode.inheritStdio;
-  ProcessStartMode executable = ProcessStartMode.detached;
-  
-  
-  print(currentDirectory);
-  var result = await Process.start(
-    'sh',
-    ['../scripts/init_linux.sh'], // 这里的路径是相对于当前工作目录的
-    // 设置脚本的工作目录
-    workingDirectory: currentDirectory!,
-    mode: processStartMode,
-  );
-  print(result.stdout);
-  print(result.stderr);
-  if (result.exitCode != 0) {
-  print('Script failed: exit code ${result.exitCode}');
-}
 }
 
-Future<void> runScript4AndoridOnLinux() async {
- String? currentDirectory;
-  
- // 加载当前包的配置信息
-  var packageConfig = await findPackageConfig(Directory.current);
-  if (packageConfig != null) {
-    // 查找指定插件的Package对象
-    var packageName = 'pytorch_dart'; 
-    var package = packageConfig.packages.firstWhereOrNull(
-      (pkg) => pkg.name == packageName,
-    );
-    if (package != null) {
+
+Future<void> setup4Android() async {
+ // 文件的URL
+  var url = Uri.parse('https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=org.pytorch&a=pytorch_android&v=2.1.0&e=aar');
+ 
+ var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var archive = ZipDecoder().decodeBytes(response.bodyBytes);
+
+    // 存储旧文件夹名到新文件夹名的映射关系
+    Map<String, String> dirRenames = {
+      'lib': 'bin',
+      'headers': 'include',
       
-      var packageRootPath = path.fromUri(package.packageUriRoot);
-      print('plugin root path:$packageRootPath');
-      
-      currentDirectory = packageRootPath;
-      print('currect directory$currentDirectory');
-    } else {
-      print('cant find $packageName');
+    };
+
+    // 循环每个文件
+    for (var file in archive) {
+      // 获得文件路径的所有组成部分
+      var filePathParts = path.split(file.name);
+
+      // 使用映射来更新文件路径部分
+      var updatedFilePathParts = filePathParts.map((part) {
+        return dirRenames[part] ?? part; // 如果映射中存在则替换，否则使用原始部分
+      }).toList();
+
+      // 重新组合文件路径
+      var filePath = path.joinAll(updatedFilePathParts);
+
+      if (file.isFile) {
+        final data = file.content as List<int>;
+        final outputPath = path.join("${Directory.current.path}/libtorch-android", filePath);
+        // 确保父目录存在
+        final directory = Directory(path.dirname(outputPath));
+        if (!directory.existsSync()) {
+          directory.createSync(recursive: true);
+        }
+        File(outputPath)..writeAsBytesSync(data);
+      } else {
+        Directory(path.join("${Directory.current.path}/libtorch-android", filePath))..createSync(recursive: true);
+      }
     }
+    print('Extraction done.');
   } else {
-    print('cant find packageConfig');
+    print('Failed to download file: ${response.statusCode}');
   }
-  
-  
-  ProcessStartMode processStartMode = ProcessStartMode.inheritStdio;
-  ProcessStartMode executable = ProcessStartMode.detached;
-  
-  
-  print(currentDirectory);
-  var result = await Process.start(
-    'sh',
-    ['../scripts/init_android_linux.sh'], // 这里的路径是相对于当前工作目录的
-    // 设置脚本的工作目录
-    workingDirectory: currentDirectory!,
-    mode: processStartMode,
-  );
-  print(result.stdout);
-  print(result.stderr);
-  if (result.exitCode != 0) {
-  print('Script failed: exit code ${result.exitCode}');
 }
-}
+
 
 void main(List<String> arguments) async{
   print('Setting up pytorch_dart...');
@@ -99,14 +102,14 @@ void main(List<String> arguments) async{
   if(command == '--platform')
   {
     if(arguments[1]=='linux'){
-  if(Platform.isLinux){
-  await runScript4Linux();
-  }
+  
+  await setup4Linux();
+  
     }
   if(arguments[1]=='android')
-  {if(Platform.isLinux){
-    await runScript4AndoridOnLinux();
-  }
+  {
+    await setup4Android();
+  
   }
   
   }
--- scripts/init_linux.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-pwd
-wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
-unzip libtorch-shared-with-deps-latest.zip
-rm libtorch-shared-with-deps-latest.zip
-
-mv libtorch libtorch-linux
-echo "Done"
