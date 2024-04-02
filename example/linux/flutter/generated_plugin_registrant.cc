0bc68b61281012bfd889d1a13ab9d2413f57f12d
KernelInterrupt <254471942@qq.com>
2024-04-02 23:39:59 +0800

    change names for some libs

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <pytorch_dart/native_pytorch_plugin.h>
+#include <pytorch_dart/pytorch_dart_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) pytorch_dart_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "NativePytorchPlugin");
-  native_pytorch_plugin_register_with_registrar(pytorch_dart_registrar);
+      fl_plugin_registry_get_registrar_for_plugin(registry, "PytorchDartPlugin");
+  pytorch_dart_plugin_register_with_registrar(pytorch_dart_registrar);
 }
